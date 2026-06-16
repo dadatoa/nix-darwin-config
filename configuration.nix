22ec84fc560646212ea13a6f8202ca41a44d40b5
dadatoa <f0a202@outlook.com>
2026-06-16 17:15:44 +0700

    migrate dadabook config

---
+++ configuration.nix
@@ -0,0 +1,47 @@
+{ pkgs
+, inputs
+, ...
+}:
+{
+  imports =
+  [
+    ./settings.nix
+    ./usefull_tools.nix
+  ];
+
+  # used for backwards compatibility
+  system.stateVersion = 5;
+
+  system.primaryUser = "dadatoa";
+
+  # fingerprint for sudo
+  security.pam.services.sudo_local.touchIdAuth = true;
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # services.tailscale.enable = true;
+  # intall with tailscale standalone version
+
+  environment.systemPackages = with pkgs; [
+    coreutils
+    cosign
+    docker
+    ext4fuse ## did not find in brew
+    eza
+    fd
+    flyctl
+    fzf
+    glow
+    lazygit
+    nmap
+    opentofu
+    rclone
+    ripgrep
+    rsync
+    tldr
+    tree-sitter
+    wishlist
+    ### LSP
+    lua-language-server    
+    ###
+  ];
+}
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }: {
+
+    darwinConfigurations."dadabook" = nix-darwin.lib.darwinSystem {
+      modules = [ ./configuration.nix ];
+    };
+
+  };
+}
